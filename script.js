9b2891d253af479a09f69539b598398f6e4ef603
Ficles <101678709+Ficles@users.noreply.github.com>
2025-10-10 18:56:44 +1300

    Update script.js

--- script.js
+++ script.js
@@ -107,3 +107,41 @@ window.addEventListener('keydown', (e) => {
 
 });
 
+window.addEventListener('touchstart', (e) => {
+    if (document.getElementsByTagName('h1').length > 0) {
+        document.getElementsByTagName('h1')[0].remove();
+    }
+    const dvd = document.createElement('img');
+    dvd.className = 'dvd-logo';
+    dvd.src = 'dvd.png';
+    dvd.style.position = 'absolute';
+    dvd.style.width = '160px';
+    dvd.style.height = '80px';
+    document.body.appendChild(dvd);
+
+    maxX = window.innerWidth - dvd.clientWidth;
+    maxY = window.innerHeight - dvd.clientHeight;
+    dvd.x2 = Math.random() * maxX;
+    dvd.y2 = Math.random() * maxY;
+    switch (Math.floor(Math.random() * 4)) {
+        case 0:
+            dvd.direction = 0.25 * Math.PI;
+            break;
+        case 1:
+            dvd.direction = 0.75 * Math.PI;
+            break;
+        case 2:
+            dvd.direction = 1.25 * Math.PI;
+            break;
+        case 3:
+            dvd.direction = 1.75 * Math.PI;
+            break;
+    }
+    dvd.direction += (Math.random() - 0.5) * 0.5;
+    dvd.style.left = `${dvd.x2}px`;
+    dvd.style.top = `${dvd.y2}px`;
+    dvd.style.filter = `invert(1) sepia(1) saturate(10000%) brightness(1.2) hue-rotate(${Math.random() * 360}deg)`;
+    dvds = document.getElementsByClassName('dvd-logo');
+});
+
+
